1e116dae1c0ac61beaa93ad474a5886393d4d3e5
danyw <danielwiejak@gmai.com>
2023-03-09 19:59:20 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Login from "./Login";
+import Logout from "./Logout";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div className="App">
+<Login />
+<Logout />
+<Profile />
+    </div>
+  );
+};
+
+export default App;
